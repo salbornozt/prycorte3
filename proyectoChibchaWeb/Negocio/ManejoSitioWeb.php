3b835426fec8c29bc0f4e125e1ac1bd782e05448
salbornozt <49846691+salbornozt@users.noreply.github.com>
2021-06-16 16:02:30 -0500

    chagues

--- proyectoChibchaWeb/Negocio/ManejoSitioWeb.php
+++ proyectoChibchaWeb/Negocio/ManejoSitioWeb.php
@@ -3,10 +3,9 @@
 /**
  * Import classes
  */
-/**require_once ("/home/u999386215/public_html/proyectoLibreria/Persistencia/ArticleDAO.php");
-    require_once ("/home/u999386215/public_html/proyectoLibreria/Persistencia/Util/Conexion.php");*/
-require_once ($_SERVER["DOCUMENT_ROOT"]) . '/proyectoChibchaWeb/persistencia/Util/Conexion.php';
-require_once ($_SERVER["DOCUMENT_ROOT"]) . '/proyectoChibchaWeb/persistencia/SitioWebDAO.php';
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/proyectoChibchaWeb/persistencia/Util/Conexion.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/proyectoChibchaWeb/persistencia/SitioWebDAO.php';
 
 
 class ManejoSitioWeb
